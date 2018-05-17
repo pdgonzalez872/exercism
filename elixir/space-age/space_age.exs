3ad1ed782dacd13e8966333e2780fbc582341a1d
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-17 10:19:25 -0500

    move exercise to correct file

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,4 +1,31 @@
 defmodule SpaceAge do
+  @planet_data [
+    earth: %{
+      days_in_year: 365.25
+    },
+    mercury: %{
+      days_in_year: 87.97
+    },
+    mars: %{
+      days_in_year: 687.0
+    },
+    venus: %{
+      days_in_year: 224.70
+    },
+    jupiter: %{
+      days_in_year: 4340
+    },
+    saturn: %{
+      days_in_year: 10_750
+    },
+    uranus: %{
+      days_in_year: 30_750
+    },
+    neptune: %{
+      days_in_year: 59_955
+    }
+  ]
+
   @type planet ::
           :mercury
           | :venus
@@ -15,5 +42,25 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    seconds
+    |> to_minutes()
+    |> to_hours()
+    |> to_days(planet)
+    |> to_years(planet)
+  end
+
+  def to_float(input), do: input / 1
+
+  def divide_by_60(input), do: input / 60
+  def to_minutes(input), do: divide_by_60(input)
+  def to_hours(input), do: divide_by_60(input)
+
+  def to_days(input, _planet) do
+    input / 24
+  end
+
+  def to_years(input, planet) do
+    planet_data = @planet_data[planet]
+    input / planet_data.days_in_year
   end
 end
