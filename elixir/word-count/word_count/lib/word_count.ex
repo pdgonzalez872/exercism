34ae0d98cb40d0fd130f3e18e4ff3d1f6cb758c4
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-22 14:51:36 -0500

    Run formatter

--- elixir/word-count/word_count/lib/word_count.ex
+++ elixir/word-count/word_count/lib/word_count.ex
@@ -1,15 +1,14 @@
 defmodule Words do
   def count(sentence) do
-
     sentence
     |> remove_punctuation()
     |> split_underscores()
     |> String.split(" ")
-    |> Enum.map(fn(el)-> String.downcase(el) end)
-    |> Enum.filter(fn(el)-> el != "" end)
-    |> Enum.reduce(%{}, fn(el, acc) ->
+    |> Enum.map(fn el -> String.downcase(el) end)
+    |> Enum.filter(fn el -> el != "" end)
+    |> Enum.reduce(%{}, fn el, acc ->
       if Map.has_key?(acc, el) do
-        Map.put(acc, "#{el}", acc["#{el}"]+1)
+        Map.put(acc, "#{el}", acc["#{el}"] + 1)
       else
         Map.put(acc, el, 1)
       end
