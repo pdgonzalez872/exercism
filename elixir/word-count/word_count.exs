028ae32218fe7e39620cb15ce2de47c23161d1fe
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-22 14:53:11 -0500

    Move files to exercism file

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,10 +1,25 @@
 defmodule Words do
-  @doc """
-  Count the number of words in the sentence.
-
-  Words are compared case-insensitively.
-  """
-  @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> remove_punctuation()
+    |> split_underscores()
+    |> String.split(" ")
+    |> Enum.map(fn el -> String.downcase(el) end)
+    |> Enum.filter(fn el -> el != "" end)
+    |> Enum.reduce(%{}, fn el, acc ->
+      if Map.has_key?(acc, el) do
+        Map.put(acc, "#{el}", acc["#{el}"] + 1)
+      else
+        Map.put(acc, el, 1)
+      end
+    end)
+  end
+
+  def remove_punctuation(sentence) do
+    Regex.replace(~r/[,!&@$%^:+]+/, sentence, "")
+  end
+
+  def split_underscores(sentence) do
+    Regex.replace(~r/[_]+/, sentence, " ")
   end
 end
