d4163b789971f7c63901fab0ddd0584101b379bc
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-22 09:42:26 -0500

    Refactor, use data transformation

--- elixir/twelve-days/twelve_days/lib/twelve_days.ex
+++ elixir/twelve-days/twelve_days/lib/twelve_days.ex
@@ -1,28 +1,55 @@
 defmodule TwelveDays do
 
+  # Looks like verses are comprised of:
+  # - Intro
+  # - descending count verses
+  # - Ending
 
-  # common ending -> "and ..."
-  # exception ending -> the first one, without "and"
+  def verse(verse_number = 1) do
+    state =
+      verse_number
+      |> get_verse_data()
+      |> build_intro()
+      |> build_ending()
 
-  def verse(1) do
-    build_verse(1)
+    Enum.join(state.output, ", ")
   end
 
+  # When given a number other than 1, must return all of the verses until 1
+  def verse(verse_number) do
+    raise "NOOO"
+    #build_verse(verse_number)
+  end
+
+  def build_intro(verse_data = %{}) do
+    intro = "On the #{verse_data.ordinal} day of Christmas my true love gave to me"
+    output = verse_data.output ++ [intro]
+    Map.put(verse_data, :output, output)
+  end
+
+  def build_ending(verse_data = %{}) do
+    ending = "#{verse_data.cardinal} #{verse_data.item}."
+    output = verse_data.output ++ [ending]
+    Map.put(verse_data, :output, output)
+  end
+
+  # no.
   def build_verse(verse_number) do
     verse_data =
       verse_number
       |> get_verse_data()
       |> Enum.at(0)
-    "On the #{verse_data.ordinal} day of Christmas my true love gave to me, #{verse_data.item}."
+    "On the #{verse_data.ordinal} day of Christmas my true love gave to me, #{verse_data.cardinal} #{verse_data.item}."
   end
 
-  defp first_verse_by_itself() do
-    "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."
+  # Add an and
+  def build_last_item() do
+    "and a Partridge in a Pear Tree"
   end
 
   def verse_list() do
     [
-      %{verse_number: 1,  cardinal: "one", ordinal: "first", item: "a Partridge in a Pear Tree"},
+      %{verse_number: 1,  cardinal: "a", ordinal: "first", item: "Partridge in a Pear Tree"},
       %{verse_number: 2,  cardinal: "two", ordinal: "second", item: "Turtle Doves"},
       %{verse_number: 3,  cardinal: "three", ordinal: "third", item: "French Hens"},
       %{verse_number: 4,  cardinal: "four", ordinal: "fourth", item: "Calling Birds"},
@@ -38,6 +65,7 @@ defmodule TwelveDays do
   end
 
   def get_verse_data(n) do
-    Enum.filter(verse_list, fn(el)-> el.verse_number == n end)
+    verse_data = Enum.filter(verse_list, fn(el)-> el.verse_number == n end) |> Enum.at(0)
+    Map.put(verse_data, :output, [])
   end
 end
