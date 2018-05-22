5791256b9a0929f86e518881d3ccba0bb16c8843
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-22 10:20:02 -0500

    Mix format

--- elixir/twelve-days/twelve_days/lib/twelve_days.ex
+++ elixir/twelve-days/twelve_days/lib/twelve_days.ex
@@ -1,11 +1,10 @@
 defmodule TwelveDays do
-
   def sing() do
     verses(1, 12)
   end
 
   def verses(start_range, end_range) do
-    Enum.map(end_range..start_range, fn(verse_number)-> verse(verse_number) end)
+    Enum.map(end_range..start_range, fn verse_number -> verse(verse_number) end)
     |> Enum.reverse()
     |> Enum.join("\n")
   end
@@ -42,6 +41,7 @@ defmodule TwelveDays do
       cond do
         state.verse_number == 1 ->
           "#{state.cardinal} #{state.item}."
+
         true ->
           verse_data = get_verse_data(1)
           "and #{verse_data.cardinal} #{verse_data.item}."
@@ -52,34 +52,35 @@ defmodule TwelveDays do
   end
 
   def build_middle_verses(state = %{}) do
-    middle = Enum.map(state.verse_number..2, fn(verse_number)->
-      verse_data = get_verse_data(verse_number)
-      "#{verse_data.cardinal} #{verse_data.item}"
-    end)
+    middle =
+      Enum.map(state.verse_number..2, fn verse_number ->
+        verse_data = get_verse_data(verse_number)
+        "#{verse_data.cardinal} #{verse_data.item}"
+      end)
 
     output = state.output ++ middle
     Map.put(state, :output, output)
   end
 
   def get_verse_data(n) do
-    state = Enum.filter(verse_list, fn(el)-> el.verse_number == n end) |> Enum.at(0)
+    state = Enum.filter(verse_list, fn el -> el.verse_number == n end) |> Enum.at(0)
     Map.put(state, :output, [])
   end
 
   def verse_list() do
     [
-      %{verse_number: 1,  cardinal: "a", ordinal: "first", item: "Partridge in a Pear Tree"},
-      %{verse_number: 2,  cardinal: "two", ordinal: "second", item: "Turtle Doves"},
-      %{verse_number: 3,  cardinal: "three", ordinal: "third", item: "French Hens"},
-      %{verse_number: 4,  cardinal: "four", ordinal: "fourth", item: "Calling Birds"},
-      %{verse_number: 5,  cardinal: "five", ordinal: "fifth", item: "Gold Rings"},
-      %{verse_number: 6,  cardinal: "six", ordinal: "sixth", item: "Geese-a-Laying"},
-      %{verse_number: 7,  cardinal: "seven", ordinal: "seventh", item: "Swans-a-Swimming"},
-      %{verse_number: 8,  cardinal: "eight", ordinal: "eighth", item: "Maids-a-Milking"},
-      %{verse_number: 9,  cardinal: "nine", ordinal: "ninth", item: "Ladies Dancing"},
+      %{verse_number: 1, cardinal: "a", ordinal: "first", item: "Partridge in a Pear Tree"},
+      %{verse_number: 2, cardinal: "two", ordinal: "second", item: "Turtle Doves"},
+      %{verse_number: 3, cardinal: "three", ordinal: "third", item: "French Hens"},
+      %{verse_number: 4, cardinal: "four", ordinal: "fourth", item: "Calling Birds"},
+      %{verse_number: 5, cardinal: "five", ordinal: "fifth", item: "Gold Rings"},
+      %{verse_number: 6, cardinal: "six", ordinal: "sixth", item: "Geese-a-Laying"},
+      %{verse_number: 7, cardinal: "seven", ordinal: "seventh", item: "Swans-a-Swimming"},
+      %{verse_number: 8, cardinal: "eight", ordinal: "eighth", item: "Maids-a-Milking"},
+      %{verse_number: 9, cardinal: "nine", ordinal: "ninth", item: "Ladies Dancing"},
       %{verse_number: 10, cardinal: "ten", ordinal: "tenth", item: "Lords-a-Leaping"},
       %{verse_number: 11, cardinal: "eleven", ordinal: "eleventh", item: "Pipers Piping"},
-      %{verse_number: 12, cardinal: "twelve", ordinal: "twelfth", item: "Drummers Drumming"},
+      %{verse_number: 12, cardinal: "twelve", ordinal: "twelfth", item: "Drummers Drumming"}
     ]
   end
 end
