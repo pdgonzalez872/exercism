aaeff97827f87436b773640176ec45d44be059ca
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-23 10:21:16 -0500

    Closer

--- elixir/acronym/acronym/lib/acronym.ex
+++ elixir/acronym/acronym/lib/acronym.ex
@@ -1,10 +1,27 @@
 defmodule Acronym do
 
+  @module_doc ~S"""
+  Nice! This challenge, coupled with a comment from an earlier exercise helped
+  me stop a pattern I found annoying (noted in an exercise before):
+
+  This pattern:
+
+  input
+  |> String.split("")
+  |> Enum.filter(fn(el)-> el != "" end)
+
+  Can be this:
+  input
+  |> String.split("", trim: true)
+
+  Awesome!
+  """
+
   def abbreviate(input) when is_binary(input) do
     input
     |> handle_punctuation()
     |> split_sentence_by_spaces()
-    |> take_first_letters_from_each_element()
+    |> take_first_letters_from_each_element() # Enum.map -> fun
     |> combine_first_letters()
   end
 
@@ -18,15 +35,27 @@ defmodule Acronym do
   def take_first_letters_from_each_element(input) do
     input
     |> Enum.map(fn(el)->
-      cond do
-        has_inconsistent_case?(el) ->
-          require IEx; IEx.pry
-          # find first upper, split there, send
-        true ->
-          [[first]] = Regex.scan(~r/^./, el)
-          String.upcase(first)
-      end
+      take_first_letter_from_element(el)
     end)
+  end
+
+  def take_first_letter_from_element(word) do
+    cond do
+      has_inconsistent_case?(word) ->
+        # find first upper, split there, send to take_first_letters_from_each_element()
+        split_word = String.split(word, "", trim: true)
+        [first | rest] = split_word
+
+        second_uppercase_letter_index = Enum.find_index(rest, fn(el)-> el == String.upcase(el) end)
+
+        Enum.slice(rest, second_uppercase_letter_index, Enum.count(rest))
+        |> Enum.join("")
+        |> take_first_letter_from_element()
+
+      true ->
+        [[first]] = Regex.scan(~r/^./, word)
+        String.upcase(first)
+    end
   end
 
   @doc """
@@ -47,8 +76,7 @@ defmodule Acronym do
   def has_inconsistent_case?(input) do
     count_of_upper =
       input
-      |> String.split("")
-      |> Enum.filter(fn(el)-> el != "" end)
+      |> String.split("", trim: true)
       |> Enum.reduce(0, fn(el, acc)->
         if el == String.upcase(el) do
           acc = acc + 1
