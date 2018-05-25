b34799e8d190bca9b83f1f8d92f41b29a35c415b
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-05-25 16:09:47 -0500

    Refactor

--- elixir/series/series/lib/series.ex
+++ elixir/series/series/lib/series.ex
@@ -1,39 +1,30 @@
 defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
 
-  def slices(binary, slice_size) when slice_size == 1 do
-    binary
-    |> String.split("", trim: true)
-    |> Enum.chunk_every(1)
-    |> List.flatten()
-  end
-
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
   def slices(_, slice_size) when slice_size <= 0, do: []
 
   def slices(binary, slice_size) do
 
     split_binary = binary |> String.split("", trim: true)
-    with_index = split_binary |> Enum.with_index()
 
-    transformation =
-      binary
-      |> String.split("", trim: true)
-      |> Enum.with_index()
-      |> Enum.map(fn {el, index} ->
+    binary
+    |> String.split("", trim: true)
+    |> Enum.with_index()
+    |> Enum.map(fn {_el, index} ->
 
-        target_index = slice_size - 1 + index
+      target_index = slice_size - 1 + index
 
-        if target_index < length(split_binary) do
-          result = Enum.reduce(index..target_index, "", fn(range_el, acc) ->
-            acc <> Enum.at(split_binary, range_el)
-          end)
-        else
-
-        end
-      end)
-      |> Enum.filter(fn el -> !is_nil(el) end)
-  end
-
-  def debug(list) do
-    require IEx; IEx.pry
+      if target_index < length(split_binary) do
+        Enum.reduce(index..target_index, "", fn(range_el, acc) ->
+          acc <> Enum.at(split_binary, range_el)
+        end)
+      end
+    end)
+    |> Enum.filter(fn el -> !is_nil(el) end)
   end
 end
