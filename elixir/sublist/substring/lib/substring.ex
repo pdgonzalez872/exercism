010453493b7e5a38ed1f83ce0fae98b7933520ad
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-05-29 11:08:29 -0500

    Almost there, will change strategy to use Regex.

--- elixir/sublist/substring/lib/substring.ex
+++ elixir/sublist/substring/lib/substring.ex
@@ -1,2 +1,31 @@
-defmodule Substring do
+defmodule Sublist do
+
+  def compare([], []), do: :equal
+  def compare([], _), do: :sublist
+  def compare(_, []), do: :superlist
+
+  def compare(a, b) do
+    cond do
+      a == b ->
+        :equal
+
+      b_is_not_contained_in_a?(a, b)->
+        :unequal
+      
+      a_is_superlist_of_b?(a, b) ->
+        :superlist
+        
+      true ->
+        :sublist
+    end
+  end
+
+  def a_is_superlist_of_b?(a, b) do
+    target_diff = length(a) - length(b)
+    length(a -- b) == target_diff
+  end
+
+  def b_is_not_contained_in_a?(a, b) do
+    Enum.count(b) == Enum.count(b -- a)
+  end
 end
