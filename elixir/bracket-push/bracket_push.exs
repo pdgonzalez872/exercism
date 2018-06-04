55c8fd1afa6ac8d8b63822223d8dd91af304cee0
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-04 17:41:36 -0500

    Move file

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -1,8 +1,74 @@
 defmodule BracketPush do
-  @doc """
-  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
-  """
-  @spec check_brackets(String.t()) :: boolean
-  def check_brackets(str) do
+  def check_brackets([]), do: false
+
+  def check_brackets(input) do
+    cleaned_input =
+      input
+      |> remove_whitespace()
+      |> remove_pairs()
+      |> select_valid_delimiters()
+
+    case cleaned_input do
+      "" ->
+        true
+
+      "[]" ->
+        true
+
+      "()" ->
+        true
+
+      "{}" ->
+        true
+
+      _ ->
+        {_first, _last, net_input} =
+          cleaned_input
+          |> String.split("", trim: true)
+          |> extract_data_from_input()
+
+        check_brackets(net_input)
+    end
+  end
+
+  def extract_data_from_input(input) do
+    first = Enum.at(input, 0)
+    last = Enum.at(input, -1)
+
+    net_input =
+      input
+      |> List.delete_at(0)
+      |> List.delete_at(-1)
+
+    {first, last, net_input}
+  end
+
+  def remove_whitespace(input) when is_binary(input) do
+    input
+    |> String.replace(" ", "")
+  end
+
+  def remove_whitespace(input) when is_list(input) do
+    if Enum.count(input) == 0 do
+      input
+    else
+      input
+      |> Enum.join("")
+      |> String.replace(" ", "")
+    end
+  end
+
+  def remove_pairs(""), do: ""
+
+  def remove_pairs(input) do
+    input
+    |> String.replace("[]", "")
+    |> String.replace("{}", "")
+    |> String.replace("()", "")
+  end
+
+  def select_valid_delimiters(input) do
+    r = ~r/[\s\d\-\+\.\&\\^*\/a-z]+/
+    Regex.replace(r, input, "")
   end
 end
