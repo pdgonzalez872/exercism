71f754e47ae81e6786f544c5bf378da05d75fc19
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-04 15:46:12 -0500

    Shameless green

--- elixir/bracket-push/bracket_push/lib/bracket_push.ex
+++ elixir/bracket-push/bracket_push/lib/bracket_push.ex
@@ -1,39 +1,78 @@
 defmodule BracketPush do
 
-
-  # def check_brackets([]), do: false
-  # def check_brackets(""), do: true
-  # def check_brackets("[]"), do: true
+  def check_brackets([]), do: false
 
   def check_brackets(input) do
+    cleaned_input =
+      input
+      |> clean_input()
+      |> remove_pairs()
+      |> filter_symbols()
 
-    case input do
+    case cleaned_input do
       "" ->
         true
 
       "[]" ->
         true
 
-      [] ->
-        false
+      "()" ->
+        true
+
+      "{}" ->
+        true
 
       _ ->
         {first, last, net_input} =
           split_input =
-            input
+            cleaned_input
             |> String.split("", trim: true)
             |> extract_data_from_input()
-
         check_brackets(net_input)
     end
   end
 
-  def extract_data_from_input(input) do
-    first = hd(input)
-    [last] = tl(input)
+  def shim(input) do
+    require IEx; IEx.pry
+  end
 
-    net_input = (input -- [first]) -- (input -- [last])
+  def extract_data_from_input(input) do
+    first = Enum.at(input, 0)
+    last = Enum.at(input, -1)
+
+    net_input =
+      input
+      |> List.delete_at(0)
+      |> List.delete_at(-1)
 
     {first, last, net_input}
+  end
+
+  def clean_input(input) when is_binary(input) do
+    input
+    |> String.replace(" ", "")
+  end
+
+  def clean_input(input) when is_list(input) do
+    if Enum.count(input) == 0 do
+      input
+    else
+      input
+      |> Enum.join("")
+      |> String.replace(" ", "")
+    end
+  end
+
+  def remove_pairs(""), do: ""
+  def remove_pairs(input) do
+    input
+    |> String.replace("[]", "")
+    |> String.replace("{}", "")
+    |> String.replace("()", "")
+  end
+
+  def filter_symbols(input) do
+    r = ~r/[\s\d\-\+\.\&\\^*\/a-z]+/
+    Regex.replace(r,input, "")
   end
 end
