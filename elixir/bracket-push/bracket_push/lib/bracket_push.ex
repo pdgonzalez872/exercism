fa2d5b3d278625feb660818cbac72af2222c133a
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-04 14:10:47 -0500

    Getting close

--- elixir/bracket-push/bracket_push/lib/bracket_push.ex
+++ elixir/bracket-push/bracket_push/lib/bracket_push.ex
@@ -10,40 +10,22 @@ defmodule BracketPush do
     case input do
       "" ->
         true
+
       "[]" ->
         true
+
       [] ->
         false
+
       _ ->
+        {first, last, net_input} =
+          split_input =
+            input
+            |> String.split("", trim: true)
+            |> extract_data_from_input()
 
-        split_input =
-          input
-          |> String.split("", trim: true)
-
-        {first, last, net_input} = extract_data_from_input(split_input)
-
-        require IEx; IEx.pry
-
-        # false
-    end
-    split_input =
-      input
-      |> String.split("", trim: true)
-
-    {first, last, net_input} = extract_data_from_input(split_input)
-
-    require IEx; IEx.pry
-    cond do
-      # even numbers input only
-
-      check_brackets(first <> last) == false ->
         check_brackets(net_input)
-
-      true ->
-        false
     end
-
-
   end
 
   def extract_data_from_input(input) do
