6a57e4447bc71aa19453e3e628385b1c562aa0e1
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-13 14:29:56 -0500

    Finish challenge

---
+++ elixir/hamming/hamming/README.md
@@ -0,0 +1,21 @@
+# Hamming
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `hamming` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:hamming, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/hamming](https://hexdocs.pm/hamming).
+
---
+++ elixir/hamming/hamming/lib/hamming.ex
@@ -0,0 +1,35 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance('', ''), do: {:ok, 0}
+
+  def hamming_distance(strand1, strand2) do
+    cond do
+      strand1 == strand2 ->
+        {:ok, 0}
+
+      Enum.count(strand1) != Enum.count(strand2) ->
+        {:error, "Lists must be the same length"}
+
+      true ->
+        require IEx; IEx.pry
+        not_equal_count =
+          Enum.zip(strand1, strand2)
+          |> Enum.reduce(0, fn({s1, s2}, acc) ->
+            if s1 != s2 do
+              acc + 1
+            else
+              acc
+            end
+          end)
+        {:ok, not_equal_count}
+    end
+  end
+end
