e408575e93aa701b8bf069229913277017663f6d
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-14 09:02:17 -0500

    Move to main file

--- elixir/isogram/isogram.exs
+++ elixir/isogram/isogram.exs
@@ -4,5 +4,16 @@ defmodule Isogram do
   """
   @spec isogram?(String.t()) :: boolean
   def isogram?(sentence) do
+    original_split =
+      Regex.run(~r/^[a-zA-Z\s]+/, sentence)
+      |> Enum.join("") # ["thisistheresult"]
+      |> String.replace(" ", "")
+      |> String.split("", trim: true) # the result here is split letters
+
+    map_set =
+      original_split
+      |> MapSet.new()
+
+    Enum.count(original_split) == Enum.count(map_set)
   end
 end
