e6ad787cb0a74a374eeca051bc3c6e795f4c046a
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-22 20:16:13 -0500

    finish, move files

--- elixir/etl/etl.exs
+++ elixir/etl/etl.exs
@@ -9,5 +9,11 @@ defmodule ETL do
   """
   @spec transform(map) :: map
   def transform(input) do
+    input
+    |> Enum.reduce(%{}, fn {k, v}, first_acc ->
+      Enum.reduce(v, %{}, fn word, second_acc ->
+        Map.merge(first_acc, Map.put(second_acc, String.downcase(word), k))
+      end)
+    end)
   end
 end
--- elixir/etl/etl_test.exs
+++ elixir/etl/etl_test.exs
@@ -3,7 +3,6 @@ if !System.get_env("EXERCISM_TEST_EXAMPLES") do
 end
 
 ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
 
 defmodule TransformTest do
   use ExUnit.Case
